5ba91157ee8c82c31297b03c1f095f3ac614d1f0
esmf-orgbot <dunlap@ucar.edu>
2021-12-15 17:57:17 +0000

    feature/neptune-sami3 ESMPy doc build by CircleCI

--- docs/nightly/feature/neptune-sami3/html/_static/documentation_options.js
+++ docs/nightly/feature/neptune-sami3/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '8.2.0 beta snapshot',
+    VERSION: '8.3.0 beta snapshot',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
